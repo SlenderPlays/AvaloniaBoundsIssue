2471c00d8111101b19dd14989a07c36507b5e6ee
Cosma George <georgemagic10@gmail.com>
2021-12-07 20:47:37 +0200

    The Commit

---
+++ Controls/CustomSlider.axaml.cs
@@ -0,0 +1,47 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace AvaloniaBoundsIssue.Controls
+{
+	public class CustomSlider : RangeBase
+	{
+		private Border? _indicator;
+
+		static CustomSlider()
+		{
+			ValueProperty.Changed.AddClassHandler<CustomSlider>((x, e) => x.UpdateIndicatorWhenPropChanged(e));
+			MinimumProperty.Changed.AddClassHandler<CustomSlider>((x, e) => x.UpdateIndicatorWhenPropChanged(e));
+			MaximumProperty.Changed.AddClassHandler<CustomSlider>((x, e) => x.UpdateIndicatorWhenPropChanged(e));
+			// Workaround that fixes all this:
+			// BoundsProperty.Changed.AddClassHandler<CustomSlider>((x, e) => x.UpdateIndicatorWhenPropChanged(e));
+		}
+
+		public CustomSlider()
+		{
+			
+		}
+
+		protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+		{
+			_indicator = e.NameScope.Get<Border>("PART_Indicator");
+
+			UpdateIndicator(Bounds.Size);
+		}
+
+		private void UpdateIndicator(Size bounds)
+		{
+			if (_indicator != null)
+			{
+				double percent = Maximum == Minimum ? 1.0 : (Value - Minimum) / (Maximum - Minimum);
+				// bounds reports all-0 entry at startup.
+				_indicator.Width = bounds.Width * percent;
+			}
+		}
+
+		private void UpdateIndicatorWhenPropChanged(AvaloniaPropertyChangedEventArgs e)
+		{
+			UpdateIndicator(Bounds.Size);
+		}
+	}
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaloniaBoundsIssue.ViewModels
+{
+	public class ViewModelBase : ReactiveObject
+	{
+	}
+}
